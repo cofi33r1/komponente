b63a4cdae3e7bbcc7310a49c30a0ccca98f3f7d5
Filip Radojicic <radojicicfilip98@gmail.com>
2020-04-21 11:03:06 +0200

    Navbar added as well as basic css

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,15 +5,23 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import React from "react";
+import PropTypes from "prop-types";
+import { useStaticQuery, graphql } from "gatsby";
 
 import "./bootstrap.min.css";
 import "./layout.css";
 
-const Layout = ({ children }) => <> {children} </>;
+import Navbar from "./global/Navbar";
 
+
+const Layout = ({ children }) => (
+<>  
+  <Navbar />
+  {children} 
+</>
+
+);
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
 }
